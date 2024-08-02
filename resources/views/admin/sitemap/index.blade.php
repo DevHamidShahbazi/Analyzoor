d8da4161586ff3144b60d29f3b83695228032d6b
DevHamidShahbazi <dev.hamidshahbazi@gmail.com>
2024-08-03 01:24:20 +0330

    clean

--- resources/views/admin/sitemap/index.blade.php
+++ resources/views/admin/sitemap/index.blade.php
@@ -7,6 +7,16 @@
     <textarea dir="ltr" class="col-12" name="test" id="test" cols="30" rows="10">
         <?xml version="1.0" encoding="UTF-8" ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            <url>
+            <loc>
+            https://fanoram.ir
+            </loc>
+            <lastmod>
+            2024-07-26T00:27:01+03:30
+            </lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.8</priority>
+            </url>
          @foreach($parentCategory as $item )
             <url>
             <loc>
@@ -36,7 +46,7 @@
         @foreach($article as $item )
             <url>
             <loc>
-            https://fanoram.ir/article/detail//{{$item->slug}}
+            https://fanoram.ir/article/detail/{{$item->slug}}
             </loc>
             <lastmod>
             {{$item->updated_at->toAtomString()}}

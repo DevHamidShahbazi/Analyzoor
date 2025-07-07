5ab62b537607ea24d30e86be584bf604596f57ed
DevHamidShahbazi <dev.hamidshahbazi@gmail.com>
2025-07-07 04:29:16 +0330

    cleanup

--- storage/framework/views/274bec246f89adf7dc1cde3bbbee78a1.php
+++
@@ -1,9 +0,0 @@
-<?php if(session('error')): ?>
-    <?php $__env->startSection('script'); ?>
-        <script type="text/javascript">
-            toastr.error("<?php echo e(session('error')); ?>", 'خطا!')
-        </script>
-        <?php session()->forget('error') ?>
-    <?php $__env->stopSection(); ?>
-<?php endif; ?>
-<?php /**PATH C:\xampp\htdocs\Analyzoor\resources\views/alert/toastr/error.blade.php ENDPATH**/ ?>

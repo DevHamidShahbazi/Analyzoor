dfcb25d030f25f5ae17f1bd0751d2d6cf8f1395e
DevHamidShahbazi <dev.hamidshahbazi@gmail.com>
2025-07-28 02:38:05 +0330

    update accordion

--- resources/views/components/public-list-chapter/index.blade.php
+++ resources/views/components/public-list-chapter/index.blade.php
@@ -1,14 +1,14 @@
 @if($chapters->count())
-    <div class="accordion mt-4" id="accordionExample">
+    <div class="accordion mt-4 modern-accordion" id="accordionExample">
         @foreach($chapters as $key => $chapter)
             @php
                 $chapterEpisodes = $chapter->episodes()->get();
                 $isCurrentChapter = isset($currentEpisode) && $chapterEpisodes->contains('id', $currentEpisode->id);
             @endphp
 
-            <div class="border border-secondary border-1 rounded-2 border-opacity-50 my-2">
-                <h5 class="accordion-header rounded-2" id="heading_episode_{{$key}}">
-                    <button dir="rtl" class="accordion-button {{ $isCurrentChapter ? '' : 'collapsed' }} rounded-2"
+            <div class="modern-accordion">
+                <h5 class="accordion-header" id="heading_episode_{{$key}}">
+                    <button dir="rtl" class="accordion-button {{ $isCurrentChapter ? '' : 'collapsed' }}"
                             type="button"
                             data-bs-toggle="collapse"
                             data-bs-target="#collapse_episode_{{$key}}"
@@ -21,7 +21,7 @@
                 </h5>
                 <div id="collapse_episode_{{$key}}" class="accordion-collapse collapse {{ $isCurrentChapter ? 'show' : '' }}"
                      aria-labelledby="heading_episode_{{$key}}" data-bs-parent="#accordionExample">
-                    <div class="accordion-body p-3">
+                    <div class="accordion-body">
                         @include('components.public-list-episode.index', [
                             'episodes' => $chapterEpisodes,
                             'currentEpisode' => $currentEpisode ?? null
--- resources/views/components/public-list-episode/index.blade.php
+++ resources/views/components/public-list-episode/index.blade.php
@@ -3,32 +3,27 @@
         $isCurrent = isset($currentEpisode) && $currentEpisode->id === $episode->id;
     @endphp
 
-    <div class="col-12 {{ $isCurrent ? 'bg-primary bg-opacity-10 rounded p-2' : '' }}">
-        <div dir="rtl" class="d-flex justify-content-between">
-            <div>
+    <div class="episode-item {{ $isCurrent ? 'current' : '' }}">
+        <div dir="rtl" class="d-flex justify-content-between align-items-center">
+            <div class="flex-grow-1">
                 <span class="fs-lg-5 fs-6 font-weight-bold">
                     جلسه {{$loop->index+1}}
                 </span>
                 <span class="mx-sm-3 mx-1">|</span>
-                <a href="{{route('episode.detail',$episode->slug)}}" class="fs-lg-5 fs-6">{{$episode->name}}</a>
+                <a href="{{route('episode.detail',$episode->slug)}}" class="episode-link fs-lg-5 fs-6">{{$episode->name}}</a>
             </div>
 
-            <div class="d-sm-block d-grid" style="min-width: fit-content">
-                <span class="fs-6">
+            <div class="d-flex align-items-center gap-3" style="min-width: fit-content">
+                <span class="episode-time fs-6">
                     <i class="fas fa-clock"></i>
                     {{$episode->time}}
                 </span>
-                <span class="mx-1 d-sm-none"></span>
-                <a href="{{route('episode.detail',$episode->slug)}}" class="btn btn-sm btn-outline-{{$episode->type == 'free' ? 'primary' : 'danger'}}">
+                <a href="{{route('episode.detail',$episode->slug)}}" class="episode-btn btn btn-sm btn-outline-{{$episode->type == 'free' ? 'primary' : 'danger'}}">
                     {{$episode->type == 'free' ? 'مشاهده' : 'نقدی'}}
                     <i class="fas fa-{{$episode->type == 'free' ? 'eye' : 'lock'}}"></i>
                 </a>
             </div>
         </div>
     </div>
-
-    @if(!$loop->last)
-        <hr>
-    @endif
 @endforeach
 
